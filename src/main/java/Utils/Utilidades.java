f6581ef7af30bfe8b4987591c3cd1878d1c0b101
ManBy <manuelblancat00@gmail.com>
2025-03-18 18:13:06 +0100

    Estructura inicial

---
+++ src/main/java/Utils/Utilidades.java
@@ -0,0 +1,52 @@
+package Utils;
+
+import java.util.Scanner;
+
+public class Utilidades {
+
+    /** Este metodo pide un string al usuario y lo valida para que no esté vacía o haya varios espacios juntos. También establece un máximo 18 caracteres.
+     * @param msn es el mensaje que recibe el usuario para indicarle qué tiene que introducir.
+     * @return devuelve la string validada
+     */
+    public static String pideString (String msn){
+        Scanner teclado = new Scanner(System.in);
+        boolean esValido = false;
+        String input;
+
+        do {
+            System.out.println(msn);
+            input = teclado.nextLine();
+
+            if (input.isEmpty()){ //comprueba si la cadena está vacía
+                System.out.println("Error. El texto no puede estar vacío.");
+            }else if (input.contains("  ")){ //comprueba si contiene más de un espacio consecutivo
+                System.out.println("Error, no se admiten espacios consecutivos.");
+            }else{
+                esValido = true;
+            }
+        }while (!esValido);
+        return input;
+    }
+
+    /** pide un numero entero al usuario y lo valida.
+     * @param msn mensaje que leerá por pantalla el usuario. Indica qué tiene que introducir.
+     * @return numero entero validado.
+     */
+    public static int pideEntero (String msn){
+        boolean esValido = false;
+        int num = 0;
+        Scanner teclado = new Scanner (System.in);
+
+        do {
+            try {
+                System.out.println(msn);
+                num = teclado.nextInt();
+                esValido = true;
+            } catch (Exception e) {
+                System.out.println("Error. Caracter no válido, solo se admiten números enteros.");
+                teclado.next();
+            }
+        } while (!esValido);
+        return num;
+    }
+}
